51884d241435ff0d07ef2d3ea8f32a99f2923514
Arya Setiawan <aryasetiawan.st@gmail.com>
2024-07-11 00:44:02 +0700

    init

---
+++ resources/views/main.blade.php
@@ -0,0 +1,37 @@
+<x-layout>
+    <section id="hero">
+        <nav>
+            
+        </nav>
+        <div>
+            <h1>KOMPETISI RENANG TANGERANG</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+            <div>
+                <button>Ikuti Kompetisi</button>
+                <a href=""><img src="" alt=""></a>
+                <a href=""><img src="" alt=""></a>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="pengenalan">
+
+    </section>
+
+    <section id="jadwal">
+
+    </section>
+
+    <section id="biaya">
+
+    </section>
+
+    <section id="petunjuk">
+
+    </section>
+
+    <footer>
+
+    </footer>
+</x-layout>
