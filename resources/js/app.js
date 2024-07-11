3e0c157120ed1e8ba8662a6aeb668db06f6af9a3
Arya Setiawan <aryasetiawan.st@gmail.com>
2024-07-11 10:15:03 +0700

    Ubah warna navbar saat scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import "./bootstrap";
+
+// Buat ubah warna navbar
+document.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    const scrollHeight = window.innerHeight - navbar.offsetHeight;
+
+    if (window.scrollY >= scrollHeight) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
