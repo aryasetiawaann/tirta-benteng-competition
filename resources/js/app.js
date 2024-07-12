b65fa8cbe2bf7c303f547e7f42a7c65c32880f98
Arya Setiawan <aryasetiawan.st@gmail.com>
2024-07-13 01:14:54 +0700

    finishing landing page & responsive tablet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,23 @@
 import "./bootstrap";
 
+document.addEventListener("click", (event) => {
+    const burger = document.querySelector(".nav-burger");
+    const close = document.querySelector(".sidebar-close");
+    const sidebar = document.querySelector(".sidebar");
+
+    if (burger && burger.contains(event.target)) {
+        if (sidebar.style.display === "block") {
+            sidebar.style.display = "none";
+        } else {
+            sidebar.style.display = "block";
+        }
+    } else if (close && close.contains(event.target)) {
+        sidebar.style.display = "none";
+    } else if (!sidebar.contains(event.target)) {
+        sidebar.style.display = "none";
+    }
+});
+
 // Buat ubah warna navbar
 document.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,7 +2,9 @@
     <section id="hero" style="background-image: linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url('{{ asset('assets/img/Swimpage.png') }}'); background-size: cover; background-position: center;">
         <nav class="navbar">
             <div class="navbar-left">
-                <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" class="logo">
+                <a href="/">
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" class="logo">
+                </a>
             </div>
             <div class="navbar-center">
                 <ul class="nav-links">
@@ -16,7 +18,19 @@
                 <button class="btn-login"> <a href="">Masuk</a></button>
                 <button class="btn-register"><a href="">Daftar</a></button>
             </div>
+            <div class="nav-burger">
+                <i class="fa fa-bars"></i>
+            </div>
         </nav>
+        <div class="sidebar">
+            <ul class="sidebar-links">
+                <li><i class="fa fa-times sidebar-close"></i></li>
+                <li><a href="#pengenalan">PENGENALAN</a></li>
+                <li><a href="#jadwal">JADWAL</a></li>
+                <li><a href="#biaya">BIAYA</a></li>
+                <li><a href="#petunjuk">PETUNJUK</a></li>
+            </ul>
+        </div>
         <div class="hero-content">
             <h1>TIRTA BENTENG SWIMMING FUN COMPETITION 2024</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
@@ -83,16 +97,59 @@
         </div>
         <h2>BIAYA KOMPETISI</h2>
         <div class="event-list">
-            <div class="event">
-                <h3>Tirta Benteng Swimming Fun Competition 2024</h3>
-            </div>
+            <ul>
+                <li class="active-event"><p>Tirta Benteng Swimming Fun Competition 2024</p></li>
+                <li><p>Tirta Benteng Swimming Fun Competition 2024</p></li>
+                <li><p>Tirta Benteng Swimming Fun Competition 2024</p></li>
+
+            </ul>
         </div>
         <div class="price-list">
             <div class="price">
                 <h3>Individu</h3>
                 <hr>
-                <h4>125.000</h4>
+                <h4>Rp. 125.000</h4>
                 <ul>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
+                </ul>
+                <a href=""><button>Daftar</button></a>
+            </div>
+            <div class="price">
+                <h3>Individu</h3>
+                <hr>
+                <h4>Rp. 125.000</h4>
+                <ul>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
+                </ul>
+                <a href=""><button>Daftar</button></a>
+            </div>
+            <div class="price">
+                <h3>Individu</h3>
+                <hr>
+                <h4>Rp. 125.000</h4>
+                <ul>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
+                    </li>
                     <li>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing  anjingelit. Dolores, ducimus.</p>
                     </li>
@@ -109,10 +166,10 @@
         <div class="petunjuk-content">
             <div class="custom-subheader">
                 <div class="line"></div>
-                <h3>Tentang Kompetisi</h3>
+                <h3>Petunjuk</h3>
                 <div class="line"></div>
             </div>
-            <h2>Karya Terbaik, Persada Sesama</h2>
+            <h2>Persyaratan Lomba</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br><br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
     </section>
@@ -121,7 +178,7 @@
         <div class="footer-container">
                 <div class="footer-left">
                     <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" class="footer-logo">
-                    <p>Kompetisi Renang Tangerang 2024</p>
+                    <p>	&#169 Tirta Banten Swimming 2024</p>
                 </div>
                 <div class="footer-menu">
                     <h4>Menu</h4>
@@ -134,8 +191,8 @@
                 </div>
                 <div class="footer-contact">
                     <h4>Hubungi Kami</h4>
-                    <p><a href="tel:0812312312312"><i class="fa fa-phone"></i> 0812312312312</a></p>
-                    <p><a href="mailto:renangtangerang@gmail.com"><i class="fa fa-envelope"></i> renangtangerang@gmail.com</a></p>
+                    <a href="tel:0812312312312"><i class="fa fa-phone"></i><span>0812312312312</span></a>
+                    <a href="mailto:renangtangerang@gmail.com"><i class="fa fa-envelope"></i><span>renangtangerang@gmail.com</span></a>
                 </div>
         </div>
     </footer>
