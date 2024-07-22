69fb81c538039334baaea099096c2aec5099f8bc
reeves63 <riffa.putra@student.umn.ac.id>
2024-07-22 16:00:40 +0700

    Perbaikan Dashboard (1)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,71 +16,7 @@
 </head>
 <body>
 
-    <div class="sidebar">
-        <div class="top">
-            <div class="logo">
-                <i class="bx bx-swim"></i>
-                <span>Lomba Renang</span>
-            </div>
-            <i class="bx bx-menu" id="btn"></i>
-        </div>
-        <div class="user">
-            <img src="{{ asset('assets/img/Burger.png') }}" alt="profile" class="user-img">
-            <div>
-                <p class="bold">John Doe</p>
-                <p>User</p>
-            </div>
-        </div>
-        <ul>
-            <li>
-                <a href="#"> 
-                    <i class="bx bxs-grid-alt"></i>
-                    <span class="nav-item">Dashboard</span>
-                </a>
-                <span class="tooltip">Dashboard</span>
-            </li>
-            <li>
-                <a href="#"> 
-                    <i class='bx bx-body'></i>
-                    <span class="nav-item">Atlet Saya</span>
-                </a>
-                <span class="tooltip">Atlet Saya</span>
-            </li>
-            <li class="dropdown-btn">
-                <a href="#"> 
-                    <i class="bx bxs-trophy"></i>
-                    <span class="nav-item">Kompetisi</span>
-                    <i class="bx bx-chevron-down"></i>
-                </a>
-                <div class="dropdown-container">
-                    <a href="#">Daftar Kompetisi</a>
-                    <a href="#">Kompetisi Saya</a>
-                </div>
-                <span class="tooltip">Kompetisi</span>
-            </li>
-            <li>
-                <a href="#"> 
-                    <i class="bx bxs-wallet"></i>
-                    <span class="nav-item">Pembayaran</span>
-                </a>
-                <span class="tooltip">Pembayaran</span>
-            </li>
-            <li>
-                <a href="#"> 
-                    <i class="bx bx-book-content"></i>
-                    <span class="nav-item">Unduhan</span>
-                </a>
-                <span class="tooltip">Unduhan</span>
-            </li>
-            <li>
-                <a href="#"> 
-                    <i class="bx bx-log-out"></i>
-                    <span class="nav-item">Logout</span>
-                </a>
-                <span class="tooltip">Logout</span>
-            </li>                                                
-        </ul>
-    </div>
+    @include('components.dashboard-sidebar')
 
     <div class="main-content">
         <div class="container">
