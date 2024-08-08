8bf0b55d9188f30da07bcb27d115b92bb74db9bb
neko-deer <124798060+neko-deer@users.noreply.github.com>
2024-08-08 20:47:23 +0700

    ketinggalan

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -78,7 +78,7 @@ return [
     'ipv6' => 'The :attribute field must be a valid IPv6 address.',
     'json' => 'The :attribute field must be a valid JSON string.',
     'list' => 'The :attribute field must be a list.',
-    'lowercase' => 'The :attribute field must be .',
+    'lowercase' => 'The :attribute field must be lowercase.',
     'lt' => [
         'array' => 'The :attribute field must have less than :value items.',
         'file' => 'The :attribute field must be less than :value kilobytes.',
