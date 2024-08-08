94f9dfc2017ed5818df1e6e900ad56d904a4948c
neko-deer <124798060+neko-deer@users.noreply.github.com>
2024-08-08 20:06:48 +0700

    club optional

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'club' => ['string', 'max:255']
+            'club' => ['nullable', 'string', 'max:255'], // Updated to make 'club' optional
         ]);
 
         $user = User::create([
