b84602c43156b9254c12c23766d883ac63a97f87
neko-deer <124798060+neko-deer@users.noreply.github.com>
2024-09-20 14:03:34 +0700

    overlay fix for 150% display(laptop)

--- resources/views/components/profile-image-preview-overlay.blade.php
+++ resources/views/components/profile-image-preview-overlay.blade.php
@@ -1,8 +1,10 @@
 <div class="profile-image-preview-overlay" id="image-preview-overlay">
     <div class="profile-image-preview-container">
-        <span id="closeOverlay" onclick="toggleOverlay(false)">×</span>
-        <h2>Preview Foto</h2>
-        <img id="preview-image" src="" alt="Image Preview" />
+        <span id="closeOverlay" class="bx bx-md bx-x" onclick="toggleOverlay(false)"></span>
+        <h2 style="margin-top:20px;" class="divider">Preview Foto</h2>
+        <div class="center">
+            <img id="preview-image" src="" alt="Image Preview">
+        </div>
         <button class="confirm-button" onclick="toggleOverlay(false)">Konfirmasi</button>
     </div>
 </div>
